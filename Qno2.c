5ff9c4e24c7077ef7888f791c2e46580b572845d
k250827-lgtm <k250827@nu.edu.pk>
2025-10-13 10:49:03 +0500

    Add files via upload

---
+++ Qno2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int std;//std means students
+	printf("enter no of students :");
+	scanf("%d",&std);
+	int marks[std];
+	int i;
+	for (i=0;i<std;i++){
+		printf("enter marks : ");
+		scanf("%d",&marks[std]);
+	}
+}
