f6396ae95dda28d2fcc68d786ec24fe7dfc450b1
k250827-lgtm <k250827@nu.edu.pk>
2025-10-14 14:02:17 +0500

    Add files via upload

---
+++ Qno2 PF.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	int marks[10];
+	int i,average,sum=0;
+	for (i=0;i<10;i++){
+		printf("enter marks : ");
+		scanf("%d",&marks[i]);
+	
+	sum+=marks[i];
+}
+	average=sum/10;
+	printf("Average of marks : %d", average);
+	return 0;
+}
